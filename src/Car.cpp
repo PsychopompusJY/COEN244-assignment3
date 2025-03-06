5bfa341ba90bb5f3604f76e3721f0dff562cbece
PsychopompusJY <163796704+PsychopompusJY@users.noreply.github.com>
2025-03-05 19:53:28 -0500

    s

---
+++ src/Car.cpp
@@ -0,0 +1,17 @@
+/*
+ * Car.cpp
+ *
+ *  Created on: Mar 5, 2025
+ *      Author: jiyongjeon
+ */
+
+
+#include <iostream>
+#include <cmath>  // For tan() and M_PI
+using namespace std;
+
+int main() {
+    int n;
+
+    return 0;
+}
